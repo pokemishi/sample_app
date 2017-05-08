3f0fb5c9276bfdea96497ac2467b7d5d9f306b50
pokemishi <yasuhiro.mishima001@croudia.co.jp>
2017-05-09 01:43:41 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def hello
-    render html: "hello, world!2"
-  end
+  include SessionsHelper
 end
