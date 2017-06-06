4e2f0817780511b9b7928a28781c5b1bbd124d66
pokemishi <yasuhiro.mishima001@croudia.co.jp>
2017-06-06 23:15:11 +0900

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,5 +12,8 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
